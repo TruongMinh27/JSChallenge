05205898d854599495b4222952190fe2b46ddfcd
TruongMinh27 <minhnct0319@dhv.edu.vn>
2022-11-11 21:10:45 +0700

    updata

---
+++ QuizApp/script.js
@@ -0,0 +1,42 @@
+const quizdata = [
+    {
+        question:'Con đường nào dẫn đến cái chết từ từ và đau đớn ?',
+        a:'Đường vào tim em ôi băng giá',
+        b:'Hút Thuốc Lá',
+        c:'Đường tình duyên đứt đoạn dây tơ',
+        d:'Đường thốt nốt',
+        correct:'b'
+    },
+    {
+        question:'Ai là Chủ Tịch Nước Việt Nam năm 2022?',
+        a:'Hồ Chí Minh',
+        b:'Võ Nguyên Giáp',
+        c:'Nguyễn Xuân Phúc',
+        d:'Trần Đại Quang',
+        correct:'c'
+    },
+    {
+        question:'Điểm cực Nam của Tổ Quốc thuộc Tỉnh nào?',
+        a:'tỉnh Hà Giang',
+        b:'tỉnh Khánh Hòa',
+        c:'tỉnh Điện Biên',
+        d:'tỉnh Cà Mau',
+        correct:'d'
+    },
+    {
+        question:'Trụ sở chính của Apple ở đâu ?',
+        a:'Houston',
+        b:'London',
+        c:'Cupertino',
+        d:'NewYork',
+        correct:'d'
+    },
+    {
+        question:'Đồng hồ thông minh của Apple còn có tên gọi là gì ?',
+        a:'SmartKey',
+        b:'SmartWatch',
+        c:'SmartPhone',
+        d:'BloxFruit',
+        correct:'b'
+    },
+]
